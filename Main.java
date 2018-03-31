ba5c26f7d0344b518b90e3f2dfb81c5f837f6b17
Akhil Khanna <31974719+akhil1213@users.noreply.github.com>
2018-03-31 00:22:20 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 import javax.swing.JOptionPane;
 class Main {
   public static void main(String[] args) {
-    String moviePicked = "The only Way";
+    String moviePicked = "The Conjuring";
     int x = moviePicked.length();
     char[] guess = new char[x];
     for(int a = 0; a < x; a++){
-      guess[a] = '_';//since guess is a string it must equal double quotes " "
-      if(moviePicked.charAt(a) == ' ') guess[a] = ' ';//the check must go after because if it was above then guess[3] wouldve again been a _
+      guess[a] = '_';//since guess is a char array it must equal double quotes ' '
+      if(moviePicked.charAt(a) == ' ') guess[a] = ' ';//the check must go after because if it was above then guess[3] wouldve again been a underscore.
     }
     Scanner sc = new Scanner(System.in);
     boolean charFound = true; //in java its boolean
-   JOptionPane.showMessageDialog(null,"This is hangman, I have picked a movie at random, you have 10 attempts good luck");
-     String underscores = "";
-   for(int i = 0; i < guess.length; i++){
+    JOptionPane.showMessageDialog(null,"This is hangman, I have picked a movie at random, you have 10 attempts good luck");
+    String underscores = "";
+    for(int i = 0; i < guess.length; i++){
        underscores = underscores+ guess[i] + " ";
-     }
+    }
     Game.hangMan( moviePicked,guess,charFound);
   }
 }
@@ -29,24 +29,24 @@ class Game{
         char x = input.charAt(0);//x is = to the first character of input line
         charFound = false;//character hasnt been found 
             for(int i = 0; i < guess.length; i++){/*this loop compares character x with each individual character in the moviePicked string and if it is equal it sets guess[i] to equal that character index of string moviePicked to make sure that the guess array looks exactly like the moviePicked string regarding to Case Sensitivity. This loop was needed since there are often similar letters in a movie and this loop checks if the character x is repeated. */
-              if(Character.toLowerCase(x) ==Character.toLowerCase(moviePicked.charAt(i))){
+              if(Character.toLowerCase(x) == Character.toLowerCase(moviePicked.charAt(i))){
                   charFound = true;//set it equal to true 
                   guess[i] = moviePicked.charAt(i);//
               }
         	  }
-            String guessArray = "";//this is how you add a character array to a string 
+        String guessArray = "";//Initialize the new guess array and then concatanate the char array to it.
             for(int i = 0; i < guess.length; i++){
             	guessArray += guess[i] + " ";
-        	    }
-          JOptionPane.showMessageDialog(null, guessArray);//prints guess character array each loop
-        if (charFound == false) y--;//if you didnt find a character you lose a turn and it would stay false since it never went inside the if statement in the for loop above
+        	  }
+        JOptionPane.showMessageDialog(null, guessArray);//Displays the current guess after each iteration.
+        if (charFound == false) y--;//Decrement y because you lose a chance if the character wasn't found.
             for(int i = 0; i < guess.length; i++){
               if(guess[i] != moviePicked.charAt(i)) guessEqualsMovie = false;
             }
-          
-          if(guessEqualsMovie == true){
-            System.out.println("You win! ");
-            break;          }
+            if(guessEqualsMovie == true){
+                System.out.println("You win! ");
+                break;
+            }
           if(y==0){
             JOptionPane.showMessageDialog(null, "You lose, the movie was: " + moviePicked);
           }
